3f54e89fb5dbe035b289c89c2f53fbe4a7e6f983
bergren2 <dan@heymrbass.com>
2014-07-30 00:35:11 -0500

    update #get_basic_data to return a hash

--- scripts/scrape.rb
+++ scripts/scrape.rb
@@ -1,28 +1,33 @@
 # More info here: https://github.com/bergren2/lp-pokemon-blue/wiki/Collecting-Data
 require 'mechanize'
 
-# Grab each name, type(s)
-def grab_basic_data
+# Grab each name, type(s) and returns as a hash
+def get_basic_data
   url = "http://bulbapedia.bulbagarden.net/wiki/Kdex"
   agent = Mechanize.new
   page = agent.get(url)
   rows = page.search(".//span[@id=\"List_of_Pok.C3.A9mon_by_Kanto_Pok.C3.A9dex_number\"]/
                       ../following-sibling::table[@align=\"center\"]/tr[@style=\"background:#FFFFFF;\"]")
+  hash = {}
 
   rows.each do |row|
+    poke_hash = {}
+    id = row.children[1].children[0].text.chomp.gsub(/^ #/, '')
     name = row.children[7].children[1].text
     type1 = row.children[9].children[1].text
     if has_type2 = row.children[11]
       type2 = has_type2.children[1].text
     end
 
-    if type2
-      puts "#{name}, #{type1}/#{type2}"
-    else
-      puts "#{name}, #{type1}"
-    end
+    poke_hash['name'] = name
+    poke_hash['type1'] = type1
+    poke_hash['type2'] = type2 if type2
+
+    hash[id] = poke_hash
   end
+
+  return hash
 end
 
 ### RUN
-grab_basic_data
+p get_basic_data
